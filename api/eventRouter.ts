f4ff2680205c12cd4e4b3cb771391248334cbbd1
navybrown1 <eddyknowledgeispower@gmail.com>
2026-05-03 21:16:07 -0400

    Return event preview data before database access

--- api/eventRouter.ts
+++ api/eventRouter.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { createRouter, authedQuery } from "./middleware";
+import { previewEvents, previewMutationResult } from "./previewData";
 import {
   findEventsByUser,
   findEventsByDateRange,
@@ -10,11 +11,19 @@ import {
 } from "./queries/events";
 
 export const eventRouter = createRouter({
-  list: authedQuery.query(({ ctx }) => findEventsByUser(ctx.user.id)),
-  upcoming: authedQuery.query(({ ctx }) => findUpcomingEvents(ctx.user.id)),
+  list: authedQuery.query(({ ctx }) =>
+    ctx.previewMode ? previewEvents : findEventsByUser(ctx.user.id),
+  ),
+  upcoming: authedQuery.query(({ ctx }) =>
+    ctx.previewMode ? previewEvents : findUpcomingEvents(ctx.user.id),
+  ),
   byRange: authedQuery
     .input(z.object({ start: z.date(), end: z.date() }))
-    .query(({ ctx, input }) => findEventsByDateRange(ctx.user.id, input.start, input.end)),
+    .query(({ ctx, input }) =>
+      ctx.previewMode
+        ? previewEvents
+        : findEventsByDateRange(ctx.user.id, input.start, input.end),
+    ),
   create: authedQuery
     .input(
       z.object({
@@ -28,7 +37,9 @@ export const eventRouter = createRouter({
         categoryId: z.number().optional(),
       }),
     )
-    .mutation(({ ctx, input }) => createEvent({ ...input, userId: ctx.user.id })),
+    .mutation(({ ctx, input }) =>
+      ctx.previewMode ? previewMutationResult : createEvent({ ...input, userId: ctx.user.id }),
+    ),
   update: authedQuery
     .input(
       z.object({
@@ -43,8 +54,12 @@ export const eventRouter = createRouter({
         categoryId: z.number().optional(),
       }),
     )
-    .mutation(({ input }) => updateEvent(input.id, input)),
+    .mutation(({ ctx, input }) =>
+      ctx.previewMode ? previewMutationResult : updateEvent(input.id, input),
+    ),
   delete: authedQuery
     .input(z.object({ id: z.number() }))
-    .mutation(({ input }) => deleteEvent(input.id)),
+    .mutation(({ ctx, input }) =>
+      ctx.previewMode ? previewMutationResult : deleteEvent(input.id),
+    ),
 });
