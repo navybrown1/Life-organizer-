dcb794f217291bd93ac629ddd296d4ec286a6bfc
navybrown1 <eddyknowledgeispower@gmail.com>
2026-05-03 21:17:48 -0400

    Return dashboard preview data before database access

--- api/dashboardRouter.ts
+++ api/dashboardRouter.ts
@@ -1,11 +1,34 @@
 import { createRouter, authedQuery } from "./middleware";
+import { previewEvents, previewGoals, previewHabits, previewTasks } from "./previewData";
 import { findTodayTasks, findUpcomingTasks, countTasksByStatus } from "./queries/tasks";
 import { findUpcomingEvents } from "./queries/events";
 import { findActiveGoals } from "./queries/goals";
 import { findHabitsByUser } from "./queries/habits";
 
+function previewStats() {
+  const todoCount = previewTasks.filter((task) => task.status === "todo").length;
+  const inProgressCount = previewTasks.filter((task) => task.status === "in_progress").length;
+  const doneCount = previewTasks.filter((task) => task.status === "done").length;
+
+  return {
+    tasks: {
+      todo: todoCount,
+      inProgress: inProgressCount,
+      done: doneCount,
+      today: previewTasks.length,
+    },
+    todayTasks: previewTasks,
+    upcomingTasks: previewTasks,
+    upcomingEvents: previewEvents,
+    activeGoals: previewGoals,
+    habits: previewHabits,
+  };
+}
+
 export const dashboardRouter = createRouter({
   stats: authedQuery.query(async ({ ctx }) => {
+    if (ctx.previewMode) return previewStats();
+
     const userId = ctx.user.id;
     const [taskStats, todayTasks, upcomingTasks, upcomingEvents, activeGoals, habits] = await Promise.all([
       countTasksByStatus(userId),
